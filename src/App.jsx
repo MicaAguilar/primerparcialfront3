e1570c5ed3f0b699c3be36e708289488ea39eb7b
Your Name <you@example.com>
2023-05-31 22:52:27 -0300

    subiendo parcial

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
 import { useState } from 'react'
-import './App.css'
+import './App.css' 
+import Card from './Components/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({
+    name: '',
+    car: ''
+  })
+
+  const [show, setShow] = useState(false)
+  const [err, setErr] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if(user.name.length >= 3 && user.car.length >= 6 && user.name[0] !== " "){
+      setShow(true)
+      setErr(false)
+    } else {
+      setErr(true)
+      setShow(false)
+    }
+  }
 
   return (
-    <>
     <div className="App">
-      <p>probando git</p>
-      <Card />
+      <h2>Elige un auto</h2>
+      <form className="Formulario" onSubmit={handleSubmit}>
+        <input 
+          className='input'
+          placeholder='Ingrese su nombre'
+          type="text"
+          onChange={(event) => setUser({...user, name: event.target.value})}/>
+        
+        <input
+          className='input'
+          placeholder='Ingrese su auto favorito'
+          type="text"
+          onChange={(event) => setUser({...user, car: event.target.value})}/>
+        
+        <button className='button'>ENVIAR</button>
+      </form>
+      {err ? <span style={{color: 'red'}}>Por favor chequea que la información sea correcta.</span> : null}
+      
+      {show ? <Card name={user.name} car={user.car}/> : null}
     </div>
-    </>
+
   )
+  
 }
 
 export default App
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const Card = () => {
-  return (
-    <div>Hola, soy una tarjeta</div>
-  )
+  const Card = ({name, car}) => {
+
+    return (
+      <div className='tarjeta'>
+          <h3>Hola {name}!</h3>
+          <h3>Sabemos que tu auto favorito es:</h3>
+          <span >{car}</span>
+      </div>
+    )
 }
 
 export default Card
